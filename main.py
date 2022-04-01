19a81c70d99a7ff0e3b4c5deed12a0045195f9ca
Artem-fp <artemboyarchenko2002@mail>
2022-04-01 16:42:45 +0300

    1234

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telebot import types
 import requests
 import bs4
 import BotGames
-from menuBot import Menu
+from menuBot import Menu, Users
 import DZ
 
 
@@ -28,6 +28,75 @@ def start(message, res=False):
                      text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке ПаЙтон".format(
                          message.from_user), reply_markup=markup)
 
+@bot.message_handler(content_types=['sticker'])
+def get_messages(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Это " + message.content_type)
+
+    sticker = message.sticker
+    bot.send_message(message.chat.id, sticker)
+
+@bot.message_handler(content_types=['audio'])
+def get_messages(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Это " + message.content_type)
+
+    audio = message.audio
+    bot.send_message(chat_id, audio)
+
+@bot.message_handler(content_types=['voice'])
+def get_messages(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Это " + message.content_type)
+
+    voice = message.voice
+    bot.send_message(message.chat.id, voice)
+
+@bot.message_handler(content_types=['photo'])
+def get_messages(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Это " + message.content_type)
+
+    photo = message.photo
+    bot.send_message(message.chat.id, photo)
+
+@bot.message_handler(content_types=['video'])
+def get_messages(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Это " + message.content_type)
+
+    video = message.video
+    bot.send_message(message.chat.id, video)
+
+
+@bot.message_handler(content_types=['document'])
+def get_messages(message):
+    chat_id = message.chat.id
+    mime_type = message.document.mime_type
+    bot.send_message(chat_id, "Это " + message.content_type + " (" + mime_type + ")")
+
+    document = message.document
+    bot.send_message(message.chat.id, document)
+    if message.document.mime_type == "video/mp4":
+        bot.send_message(message.chat.id, "This is a GIF!")
+
+@bot.message_handler(content_types=['location'])
+def get_messages(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Это " + message.content_type)
+
+    location = message.location
+    bot.send_message(message.chat.id, location)
+
+@bot.message_handler(content_types=['contact'])
+def get_messages(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Это " + message.content_type)
+
+    contact = message.contact
+    bot.send_message(message.chat.id, contact)
+
+
 
 # -----------------------------------------------------------------------
 # Получение сообщений от юзера
@@ -35,6 +104,10 @@ def start(message, res=False):
 def get_text_messages(message):
     chat_id = message.chat.id
     ms_text = message.text
+
+    cur_user = Users.getUser(chat_id)
+    if cur_user == None:
+        cur_user = Users(chat_id, message.json["from"])
 
     if ms_text == "Главное меню" or ms_text == "👋 Главное меню" or ms_text == "Вернуться в главное меню":  # ..........
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -49,24 +122,30 @@ def get_text_messages(message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton("Прислать собаку")
         btn2 = types.KeyboardButton("Прислать анекдот")
+        btn3 = types.KeyboardButton("Прислать лисичку")
         back = types.KeyboardButton("Вернуться в главное меню")
-        markup.add(btn1, btn2, back)
+        markup.add(btn1, btn2, btn3, back)
         bot.send_message(chat_id, text="Развлечения", reply_markup=markup)
 
-    elif ms_text == "/dog" or ms_text == "Прислать собаку":  # .........................................................
-        contents = requests.get('https://random.dog/woof.json').json()
-        urlDOG = contents['url']
-        bot.send_photo(chat_id, photo=urlDOG, caption="Вот тебе собачка!!!")
 
-    elif ms_text == "Прислать анекдот":  # .............................................................................
-        def get_anekdot():
-            array_anekdots = []
-            req_anek = requests.get('http://anekdotme.ru/random')
-            soup = bs4.BeautifulSoup(req_anek.text, "html.parser")
-            result_find = soup.select('.anekdot_text')
-            for result in result_find:
-                array_anekdots.append(result.getText().strip())
-            return array_anekdots[0]
+    elif ms_text == "Прислать собаку":
+
+        bot.send_photo(chat_id, photo=get_dogURL(), caption="Вот тебе собачка!!!")
+
+    elif ms_text == "Прислать лисичку":
+
+        bot.send_photo(chat_id, photo=get_foxURL(), caption="Вот тебе лисичка!!!")
+
+
+    elif ms_text == "Прислать анекдот":
+
+        bot.send_message(chat_id, text=get_anekdot())
+
+
+
+
+
+
 
     elif ms_text == "WEB-камера":
         bot.send_message(chat_id, text="еще не готово...")
@@ -84,6 +163,54 @@ def get_text_messages(message):
 
     else:  # ...........................................................................................................
         bot.send_message(chat_id, text="Я тебя слышу!!! Ваше сообщение: " + ms_text)
+
+def get_anekdot():
+    array_anekdots = []
+    req_anek = requests.get('http://anekdotme.ru/random')
+    if req_anek.status_code == 200:
+        soup = bs4.BeautifulSoup(req_anek.text, "html.parser")
+        result_find = soup.select('.anekdot_text')
+        for result in result_find:
+            array_anekdots.append(result.getText().strip())
+    if len(array_anekdots) > 0:
+        return array_anekdots[0]
+    else:
+        return ""
+
+# -----------------------------------------------------------------------
+def get_news():
+    array_anekdots = []
+    req_anek = requests.get('https://www.banki.ru/news/lenta')
+    if req_anek.status_code == 200:
+        soup = bs4.BeautifulSoup(req_anek.text, "html.parser")
+        result_find = soup.select('.doFpcq')
+        for result in result_find:
+            print(result)
+
+
+
+            # array_anekdots.append(result.getText().strip())
+    if len(array_anekdots) > 0:
+        return array_anekdots[0]
+    else:
+        return ""
+
+def get_dogURL():
+    url = ""
+    req = requests.get('https://random.dog/woof.json')
+    if req.status_code == 200:
+        r_json = req.json()
+        url = r_json['url']
+        # url.split("/")[-1]
+    return url
+def get_foxURL():
+    url = ""
+    req = requests.get('https://randomfox.ca/floof/')
+    if req.status_code == 200:
+        r_json = req.json()
+        url = r_json['image']
+        # url.split("/")[-1]
+    return url
 
 # -----------------------------------------------------------------------
 bot.polling(none_stop=True, interval=0) # Запускаем бота
