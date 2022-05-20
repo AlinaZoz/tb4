3e0930b75c073a7450eea1ed2bfde53cbd81f011
Evelina655 <eva_omar@mail.ru>
2022-05-20 16:01:08 +0300

    1234

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-from io import BytesIO
-
 import telebot  # pyTelegramBotAPI 4.3.1
 from telebot import types
-import requests
-import bs4
-import botGames
-from menuBot import Menu, Users
-import DZ
+import botGames  # бот-игры, файл botGames.py
+import menuBot
+from menuBot import Menu  # в этом модуле есть код, создающий экземпляры классов описывающих моё меню
+import DZ  # домашнее задание от первого урока
+import fun  # развлечения
+import speech  # работа с речью
+
 
 bot = telebot.TeleBot('5220552349:AAGUwS1OQLApB0Lovm7AWNDK75GKCXNZf3Q')
 
 
 # -----------------------------------------------------------------------
 @bot.message_handler(commands="start")
-def command(message, res=False):
+def command(message):
     chat_id = message.chat.id
     bot.send_sticker(chat_id, "CAACAgIAAxkBAAIaeWJEeEmCvnsIzz36cM0oHU96QOn7AAJUAANBtVYMarf4xwiNAfojBA")
     txt_message = f"Привет, {message.from_user.first_name}! Я тестовый бот для курса программирования на языке Python"
     bot.send_message(chat_id, text=txt_message, reply_markup=Menu.getMenu(chat_id, "Главное меню").markup)
 
 
+
 # -----------------------------------------------------------------------
+# Получение стикеров от юзера
 @bot.message_handler(content_types=['sticker'])
 def get_messages(message):
     chat_id = message.chat.id
@@ -36,6 +38,7 @@ def get_messages(message):
 
 
 # -----------------------------------------------------------------------
+# Получение аудио от юзера
 @bot.message_handler(content_types=['audio'])
 def get_messages(message):
     chat_id = message.chat.id
@@ -44,17 +47,24 @@ def get_messages(message):
     audio = message.audio
     bot.send_message(chat_id, audio)
 
+
 # -----------------------------------------------------------------------
+# Получение голосовухи от юзера
 @bot.message_handler(content_types=['voice'])
 def get_messages(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, "Это " + message.content_type)
 
     voice = message.voice
-    bot.send_message(message.chat.id, voice)
+    # bot.send_message(message.chat.id, voice)
 
+    import speech
+    fileInfo = bot.get_file(voice.file_id)
+    audioData = bot.download_file(fileInfo.file_path)
+    bot.send_message(chat_id, speech.getTextFromVoice(audioData))
 
 # -----------------------------------------------------------------------
+# Получение фото от юзера
 @bot.message_handler(content_types=['photo'])
 def get_messages(message):
     chat_id = message.chat.id
@@ -65,6 +75,7 @@ def get_messages(message):
 
 
 # -----------------------------------------------------------------------
+# Получение видео от юзера
 @bot.message_handler(content_types=['video'])
 def get_messages(message):
     chat_id = message.chat.id
@@ -75,6 +86,7 @@ def get_messages(message):
 
 
 # -----------------------------------------------------------------------
+# Получение документов от юзера
 @bot.message_handler(content_types=['document'])
 def get_messages(message):
     chat_id = message.chat.id
@@ -86,7 +98,9 @@ def get_messages(message):
     if message.document.mime_type == "video/mp4":
         bot.send_message(message.chat.id, "This is a GIF!")
 
+
 # -----------------------------------------------------------------------
+# Получение координат от юзера
 @bot.message_handler(content_types=['location'])
 def get_messages(message):
     chat_id = message.chat.id
@@ -95,8 +109,14 @@ def get_messages(message):
     location = message.location
     bot.send_message(message.chat.id, location)
 
+    from Weather import WeatherFromPyOWN
+    pyOWN = WeatherFromPyOWN()
+    bot.send_message(chat_id, pyOWN.getWeatherAtCoords(location.latitude, location.longitude))
+    bot.send_message(chat_id, pyOWN.getWeatherForecastAtCoords(location.latitude, location.longitude))
+
 
 # -----------------------------------------------------------------------
+# Получение контактов от юзера
 @bot.message_handler(content_types=['contact'])
 def get_messages(message):
     chat_id = message.chat.id
@@ -107,147 +127,72 @@ def get_messages(message):
 
 
 # -----------------------------------------------------------------------
+# Получение сообщений от юзера
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-
     chat_id = message.chat.id
     ms_text = message.text
 
-    cur_user = Users.getUser(chat_id)
-    if cur_user == None:
-        cur_user = Users(chat_id, message.json["from"])
+    cur_user = menuBot.Users.getUser(chat_id)
+    if cur_user is None:
+        cur_user = menuBot.Users(chat_id, message.json["from"])
 
-    result = goto_menu(chat_id, ms_text)  # попытаемся использовать текст как команду меню, и войти в него
-    if result == True:
+    # проверка = мы нажали кнопку подменю, или кнопку действия
+    subMenu = menuBot.goto_menu(bot, chat_id, ms_text)  # попытаемся использовать текст как команду меню, и войти в него
+    if subMenu is not None:
+        # Проверим, нет ли обработчика для самого меню. Если есть - выполним нужные команды
+        if subMenu.name == "Игра в 21":
+            game21 = botGames.newGame(chat_id, botGames.Game21(jokers_enabled=True))  # создаём новый экземпляр игры
+            text_game = game21.get_cards(2)  # просим 2 карты в начале игры
+            bot.send_media_group(chat_id, media=game21.mediaCards)  # получим и отправим изображения карт
+            bot.send_message(chat_id, text=text_game)
+
+        elif subMenu.name == "Игра КНБ":
+            gameRPS = botGames.newGame(chat_id, botGames.GameRPS())  # создаём новый экземпляр игры и регистрируем его
+            bot.send_photo(chat_id, photo=gameRPS.url_picRules, caption=gameRPS.text_rules, parse_mode='HTML')
+
         return  # мы вошли в подменю, и дальнейшая обработка не требуется
 
+    # проверим, является ли текст текущий команды кнопкой действия
     cur_menu = Menu.getCurMenu(chat_id)
-    if cur_menu != None and ms_text in cur_menu.buttons:  # проверим, что команда относится к текущему меню
+    if cur_menu is not None and ms_text in cur_menu.buttons:  # проверим, что команда относится к текущему меню
+        module = cur_menu.module
+
+        if module != "":  # проверим, есть ли обработчик для этого пункта меню в другом модуле, если да - вызовем его (принцип инкапсуляции)
+            exec(module + ".get_text_messages(bot, cur_user, message)")
 
         if ms_text == "Помощь":
-            send_help(chat_id)
+            send_help(bot, chat_id)
 
-        elif ms_text == "Прислать собачку":
-            bot.send_photo(chat_id, photo=get_dogURL(), caption="Вот тебе собачка!")
-
-        elif ms_text == "Прислать лисичку":
-            bot.send_photo(chat_id, photo=get_foxURL(), caption="Вот тебе лисичка!")
-
-        elif ms_text == "Aнекдот":
-            # bot.send_message(chat_id, text=get_anekdot())
-            bot.send_message(chat_id, text=get_news())
-
-
-        elif ms_text == "Прислать фильм":
-            send_film(chat_id)
-
-        elif ms_text == "Угадай кто?":
-            get_ManOrNot(chat_id)
-
-        elif ms_text == "Карту!":
-            game21 = botGames.getGame(chat_id)
-            if game21 == None:  # если мы случайно попали в это меню, а объекта с игрой нет
-                goto_menu(chat_id, "Выход")
-                return
-
-            text_game = game21.get_cards(1)
-            bot.send_media_group(chat_id, media=getMediaCards(game21))  # получим и отправим изображения карт
-            bot.send_message(chat_id, text=text_game)
-
-            if game21.status != None:  # выход, если игра закончена
-                botGames.stopGame(chat_id)
-                goto_menu(chat_id, "Выход")
-                return
-
-        elif ms_text == "Стоп!":
-            botGames.stopGame(chat_id)
-            goto_menu(chat_id, "Выход")
-            return
-
-
-        elif ms_text in botGames.GameRPS.values:  # реализация игрыы Камень-ножницы-бумага
-            gameRSP = botGames.getGame(chat_id)
-            if gameRSP == None:  # если мы случайно попали в это меню, а объекта с игрой нет
-                goto_menu(chat_id, "Выход")
-                return
-            text_game = gameRSP.playerChoice(ms_text)
-            bot.send_message(chat_id, text=text_game)
-            gameRSP.newGame()
-
-        elif ms_text == "Задание-1":
-            DZ.dz1(bot, chat_id)
-
-        elif ms_text == "Задание-2":
-            DZ.dz2(bot, chat_id)
-
-        elif ms_text == "Задание-3":
-            DZ.dz3(bot, chat_id)
-
-        elif ms_text == "Задание-4":
-            DZ.dz4(bot, chat_id)
-
-        elif ms_text == "Задание-5":
-            DZ.dz5(bot, chat_id)
-
-        elif ms_text == "Задание-6":
-            DZ.dz6(bot, chat_id)
-
-    else:  # ...........................................................................................................
+    else:  # ======================================= случайный текст
         bot.send_message(chat_id, text="Мне жаль, я не понимаю вашу команду: " + ms_text)
-        goto_menu(chat_id, "Главное меню")
+        menuBot.goto_menu(bot, chat_id, "Главное меню")
+
 
 # -----------------------------------------------------------------------
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    # если требуется передать параметр или несколько параметров в обработчик кнопки, использовать методы Menu.getExtPar() и Menu.setExtPar()
-    pass
-    # if call.data == "ManOrNot_GoToSite": #call.data это callback_data, которую мы указали при объявлении InLine-кнопки
-    #
-    #     # После обработки каждого запроса нужно вызвать метод answer_callback_query, чтобы Telegram понял, что запрос обработан.
-    #     bot.answer_callback_query(call.id)
+    # если требуется передать один или несколько параметров в обработчик кнопки,
+    # используйте методы Menu.getExtPar() и Menu.setExtPar()
+    # call.data это callback_data, которую мы указали при объявлении InLine-кнопки
+    # После обработки каждого запроса вызовете метод answer_callback_query(), чтобы Telegram понял, что запрос обработан
+    chat_id = call.message.chat.id
+    message_id = call.message.id
+    cur_user = menuBot.Users.getUser(chat_id)
+    if cur_user is None:
+        cur_user = menuBot.Users(chat_id, call.message.json["from"])
 
-# -----------------------------------------------------------------------
-def goto_menu(chat_id, name_menu):
-    cur_menu = Menu.getCurMenu(chat_id)
-    if name_menu == "Выход" and cur_menu != None and cur_menu.parent != None:
-        target_menu = Menu.getMenu(chat_id, cur_menu.parent.name)
-    else:
-        target_menu = Menu.getMenu(chat_id, name_menu)
+    tmp = call.data.split("|")
+    menu = tmp[0] if len(tmp) > 0 else ""
+    cmd = tmp[1] if len(tmp) > 1 else ""
+    par = tmp[2] if len(tmp) > 2 else ""
 
-    if target_menu != None:
-        bot.send_message(chat_id, text=target_menu.name, reply_markup=target_menu.markup)
-
-
-        if target_menu.name == "Игра в 21":
-            game21 = botGames.newGame(chat_id, botGames.Game21(jokers_enabled=True))
-            text_game = game21.get_cards(2)
-            bot.send_media_group(chat_id, media=getMediaCards(game21))
-            bot.send_message(chat_id, text=text_game)
-
-        elif target_menu.name == "Камень, ножницы, бумага":
-            gameRSP = botGames.newGame(chat_id, botGames.GameRPS())
-            text_game = "<b>Победитель определяется по следующим правилам:</b>\n" \
-                        "1. Камень побеждает ножницы\n" \
-                        "2. Бумага побеждает камень\n" \
-                        "3. Ножницы побеждают бумагу"
-            bot.send_photo(chat_id, photo="https://i.ytimg.com/vi/Gvks8_WLiw0/maxresdefault.jpg", caption=text_game, parse_mode='HTML')
-
-        return True
-    else:
-        return False
+    if menu == "GameRPSm":
+        botGames.callback_worker(bot, cur_user, cmd, par, call)  # обработчик кнопок игры находится в модули игры
 
 
 # -----------------------------------------------------------------------
-def getMediaCards(game21):
-    medias = []
-    for url in game21.arr_cards_URL:
-        medias.append(types.InputMediaPhoto(url))
-    return medias
-
-
-# -----------------------------------------------------------------------
-def send_help(chat_id):
-    global bot
+def send_help(bot, chat_id):
     bot.send_message(chat_id, "Автор: Швец Андрей")
     markup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton(text="Напишите автору", url="https://t.me/user59387")
@@ -256,119 +201,10 @@ def send_help(chat_id):
     bot.send_photo(chat_id, img, reply_markup=markup)
 
     bot.send_message(chat_id, "Активные пользователи чат-бота:")
-    for el in Users.activeUsers:
-        bot.send_message(chat_id, Users.activeUsers[el].getUserHTML(), parse_mode='HTML')
+    for el in menuBot.Users.activeUsers:
+        bot.send_message(chat_id, menuBot.Users.activeUsers[el].getUserHTML(), parse_mode='HTML')
 
-# -----------------------------------------------------------------------
-def send_film(chat_id):
-    film = get_randomFilm()
-    info_str = f"<b>{film['Наименование']}</b>\n" \
-               f"Год: {film['Год']}\n" \
-               f"Страна: {film['Страна']}\n" \
-               f"Жанр: {film['Жанр']}\n" \
-               f"Продолжительность: {film['Продолжительность']}"
-    markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton(text="Трейлер", url=film["Трейлер_url"])
-    btn2 = types.InlineKeyboardButton(text="СМОТРЕТЬ онлайн", url=film["фильм_url"])
-    markup.add(btn1, btn2)
-    bot.send_photo(chat_id, photo=film['Обложка_url'], caption=info_str, parse_mode='HTML', reply_markup=markup)
-
-
-# -----------------------------------------------------------------------
-def get_anekdot():
-    array_anekdots = []
-    req_anek = requests.get('http://anekdotme.ru/random')
-    if req_anek.status_code == 200:
-        soup = bs4.BeautifulSoup(req_anek.text, "html.parser")
-        result_find = soup.select('.anekdot_text')
-        for result in result_find:
-            array_anekdots.append(result.getText().strip())
-    if len(array_anekdots) > 0:
-        return array_anekdots[0]
-    else:
-        return ""
-
-# -----------------------------------------------------------------------
-def get_news():
-    array_anekdots = []
-    req_anek = requests.get('https://www.banki.ru/news/lenta')
-    if req_anek.status_code == 200:
-        soup = bs4.BeautifulSoup(req_anek.text, "html.parser")
-        result_find = soup.select('.doFpcq')
-        for result in result_find:
-            print(result)
-
-
-
-            # array_anekdots.append(result.getText().strip())
-    if len(array_anekdots) > 0:
-        return array_anekdots[0]
-    else:
-        return ""
-
-# -----------------------------------------------------------------------
-def get_foxURL():
-    url = ""
-    req = requests.get('https://randomfox.ca/floof/')
-    if req.status_code == 200:
-        r_json = req.json()
-        url = r_json['image']
-        # url.split("/")[-1]
-    return url
-# -----------------------------------------------------------------------
-def get_dogURL():
-    url = ""
-    req = requests.get('https://random.dog/woof.json')
-    if req.status_code == 200:
-        r_json = req.json()
-        url = r_json['url']
-        # url.split("/")[-1]
-    return url
-# -----------------------------------------------------------------------
-def get_ManOrNot(chat_id):
-    global bot
-
-    markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton(text="Проверить", url="https://vc.ru/dev/58543-thispersondoesnotexist-sayt-generator-realistichnyh-lic")
-    markup.add(btn1)
-
-    req = requests.get("https://thispersondoesnotexist.com/image", allow_redirects=True)
-    if req.status_code == 200:
-        img = BytesIO(req.content)
-        bot.send_photo(chat_id, photo=img, reply_markup=markup, caption="Этот человек реален?")
-
-# ---------------------------------------------------------------------
-def get_randomFilm():
-    url = 'https://randomfilm.ru/'
-    infoFilm = {}
-    req_film = requests.get(url)
-    soup = bs4.BeautifulSoup(req_film.text, "html.parser")
-    result_find = soup.find('div', align="center", style="width: 100%")
-    infoFilm["Наименование"] = result_find.find("h2").getText()
-    names = infoFilm["Наименование"].split(" / ")
-    infoFilm["Наименование_rus"] = names[0].strip()
-    if len(names) > 1:
-        infoFilm["Наименование_eng"] = names[1].strip()
-
-    images = []
-    for img in result_find.findAll('img'):
-        images.append(url + img.get('src'))
-    infoFilm["Обложка_url"] = images[0]
-
-    details = result_find.findAll('td')
-    infoFilm["Год"] = details[0].contents[1].strip()
-    infoFilm["Страна"] = details[1].contents[1].strip()
-    infoFilm["Жанр"] = details[2].contents[1].strip()
-    infoFilm["Продолжительность"] = details[3].contents[1].strip()
-    infoFilm["Режиссёр"] = details[4].contents[1].strip()
-    infoFilm["Актёры"] = details[5].contents[1].strip()
-    infoFilm["Трейлер_url"] = url + details[6].contents[0]["href"]
-    infoFilm["фильм_url"] = url + details[7].contents[0]["href"]
-
-    return infoFilm
 # ---------------------------------------------------------------------
 
 
 bot.polling(none_stop=True, interval=0)  # Запускаем бота
-
-print()
